519bfa1c1a70e277caea044603540b12ca9f0f93
Lukas Berger <mail@lukasberger.at>
2017-06-14 22:36:20 +0200

    zero: create unified device-tree for Lineage/AOKP
    Change-Id: I65566ac9e115d4a22fd3bc0d5688e32db6ff3dbb

---
+++ aokp.mk
@@ -0,0 +1,18 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zeroltexx/full_zeroltexx.mk)
+
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Enhanced NFC
+# $(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="Galaxy S6 Edge" \
+    TARGET_DEVICE="Galaxy S6 Edge"
+
+PRODUCT_NAME := aokp_zeroltexx
+PRODUCT_DEVICE := zeroltexx
