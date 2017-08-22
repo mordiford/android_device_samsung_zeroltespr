1cbb411474e38e1a3be8c922ff9755668cf47f92
Lukas0610 <mail@lukasberger.at>
2017-08-22 02:37:38 +0200

    zero: added support for pure AOSP

---
+++ aosp_zeroltexx.mk
@@ -0,0 +1,21 @@
+# Initialise device config
+$(call inherit-product, device/samsung/zeroltexx/full_zeroltexx.mk)
+
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
+
+# Enhanced NFC
+# $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit properties for TeamNexus-bases ROMs
+$(call inherit-product, vendor/nexus/product.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="zeroltexx" \
+    TARGET_DEVICE="zerolte"
+
+PRODUCT_NAME := aosp_zeroltexx
+PRODUCT_DEVICE := zeroltexx
